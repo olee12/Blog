0dcc60f6d6d81481e41f18edf87ab564731f9aa0
olee12 <th160887@gmail.com>
2017-08-03 18:24:29 +0600

    Rendering Posts

--- blog/app/Http/Controllers/PostsController.php
+++ blog/app/Http/Controllers/PostsController.php
@@ -10,14 +10,16 @@ class PostsController extends Controller
     //
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::orderBy('created_at','dest')->get();
+        //$posts = Post::latest()->get();
 
     	return view('posts.index',compact('posts'));
     }
 
-    public function show()
+    public function show(Post $post)
     {
-        return view('posts.show');
+       // $post = Post::find($id);
+        return view('posts.show',compact('post'));
     }
     public function create()
     {
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -3,7 +3,7 @@
 Route::get( '/' , 'PostsController@index' );
 Route::get( '/posts/create' , 'PostsController@create' );
 Route::post('/posts','PostsController@store');
-//Route::get( '/posts/{post}' , 'PostsController@show' );
+Route::get( '/posts/{post}' , 'PostsController@show' );
 
 /**
 Route::get('/',function (){
