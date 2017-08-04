615fbdd65673eeaedd0fc296c589c07e1ee726f2
olee12 <th160887@gmail.com>
2017-08-04 09:36:02 +0600

    Associating With Users

--- blog/app/Post.php
+++ blog/app/Post.php
@@ -5,8 +5,25 @@ namespace App;
 
 class Post extends Model
 {
-    public function comments(){
+    public function comments()
+    {
         return $this->hasMany(Comment::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function addComment($body)
+    {
+
+        $this->comments()->create(compact('body'));
+
+        /*Comment::create([
+            'body' => $body,
+            'post_id' => $this->id
+        ]);*/
     }
 
 }
--- blog/resources/views/posts/show.blade.php
+++ blog/resources/views/posts/show.blade.php
@@ -19,6 +19,30 @@
             @endforeach
             </ul>
         </div>
+
+        {{--add a comment --}}
+
+        <hr>
+
+        <div class="card">
+            <div class="card-block">
+
+                <form method="POST" action="/posts/{{$post->id}}/comments">
+                    {{csrf_field()}}
+                    <div class="form-group">
+                        <textarea name="body" placeholder="Your Comment here" class="form-control" required></textarea>
+
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Add Comment</button>
+                        </div>
+                    @include('layouts.errors')
+                    </div>
+                </form>
+
+
+            </div>
+        </div>
+
     </div>
 
 @endsection
